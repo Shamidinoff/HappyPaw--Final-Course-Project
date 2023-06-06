dc9c57e63b3ffc7a1e5f2b4cbd1497e8a2a88a54
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-06-06 20:19:45 +0600

    index.css

---
+++ client/src/pages/Ayazhan/blog1/Blog1.js
@@ -0,0 +1,340 @@
+import Footer from "../../../components/footer/Footer";
+import Header from "../../../components/header/Header";
+import "./blog1.css";
+
+function Blog1() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <div class="nav1_blog">
+          <div class="container">
+            <div>
+              <a class="main" href="#">
+                Главная
+              </a>
+              <img src="public/images-blog/right1.svg" alt="right1" />
+              <a class="blog" href="#">
+                Блог
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="nav2_blog">
+            <img src="../../../../images-blog/down.svg" alt="down" />
+            <a href="#">Последние</a>
+          </div>
+
+          <div class="cards_blog">
+            <div class="card_blog">
+              <img src="/public/images-blog/card1.png" alt="img_card1" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+
+                <p>25 Августа, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>181</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h1></h1>
+                <h2>Зачем кошке стрижка?</h2>
+                <p>
+                  Кошачьи стрижки бывают двух видов: гигиенические и модельные.
+                  Выполняют их обученные...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card2.png" alt="img_card2" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>20 Августа, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>98</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Животное из приюта</h2>
+                <p>
+                  Если вы хотя бы немного интересовались судьбами бездомных
+                  животных, то при выборе домашнего...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card3.png" alt="img_card3" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>14 Августа, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>101</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Почему собака воет?</h2>
+                <p>
+                  С помощью голоса собака общается, проявляет свои эмоции,
+                  чувства, недовольства, высказывает...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card4.png" alt="img_card4" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>09 Августа, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>135</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Собачий корм для кошек</h2>
+                <p>
+                  У кошек и собак разные потребности в витаминах, минералах и
+                  составе потребляемой пищи...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card5.png" alt="img_card5" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>01 Августа, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>250</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>COVID у животных</h2>
+                <p>
+                  Сейчас весь мир находится в опасении страшного вируса, который
+                  приостановил мегаполисы, закрыл...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img
+                class="card_blog_img"
+                src="/public/images-blog/card6.png"
+                alt="img_card6"
+              />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>31 Июля, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>230</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Лакомство для собак</h2>
+                <p>
+                  Для чего нужны лакомства для собак? Каждому хохяину хочется
+                  побаловать своего друга. Многие могут дать им...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card7.png" alt="img_card7" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>28 Июля, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>280</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Породы котов</h2>
+                <p>
+                  Как выбрать породистую кошку? Для начала задумайтесь, какого
+                  котенка вы бы хотели взять и для...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card8.png" alt="img_card8" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>20 Июля, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>190</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Аптечка для питомца</h2>
+                <p>
+                  Если с вами живет питомец, то и ветаптечка тоже. Но, конечно,
+                  скупать все лекарства подряд нет смысла...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="card_blog">
+              <img src="/public/images-blog/card9.png" alt="img_card9" />
+
+              <div class="review_card">
+                <img
+                  class="calendar"
+                  src="/public/images-blog/calendar.svg"
+                  alt="calendar"
+                />
+                <p>19 Августа, 2022</p>
+
+                <div class="eye_on">
+                  <img src="/public/images-blog/eye on.svg" alt="eye on" />
+                  <p>200</p>
+                </div>
+              </div>
+
+              <div class="question_answer">
+                <h2>Этикет выгула собак</h2>
+                <p>
+                  Увы, не в каждом районе есть отдельная собачья площадка и
+                  возможность для бега без поводка...
+                </p>
+
+                <div class="nav_card">
+                  <a href="#">Читать</a>
+                  <img src="/public/images-blog/right2.svg" alt="right2" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="pagination_blog">
+            <img src="/public/images-blog/left1.svg" alt="left1" />
+
+            <div class="buttons_nav3">
+              <button class="button_active_nav3">1</button>
+
+              <a>2</a>
+              <a>3</a>
+              <a>4</a>
+              <a>5</a>
+              <a>...</a>
+              <a>12</a>
+            </div>
+
+            <img src="/public/images-blog/right3.svg" alt="right3" />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Blog1;
